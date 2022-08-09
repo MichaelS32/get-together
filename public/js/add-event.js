1a8c303e821538a5c1aaee03417ee32b0360c94e
mschneider <michaelschneider3232@gmail.com>
2022-08-09 08:37:18 -0500

    added public js files

--- public/js/add-event.js
+++ public/js/add-event.js
@@ -1,5 +1,25 @@
 async function newFormHandler(event) {
     event.preventDefault();
 
-    const title = document.querySelector('input')
-}
+    const title = document.querySelector('input[name="event-title"]').value;
+    const content = document.querySelector('input[name="content"').value;
+
+    const response = await fetch('/api/events', {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            content
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText)
+    }
+};
+
+document.querySelector('#new-event-form').addEventListener('submit', newFormHandler);
