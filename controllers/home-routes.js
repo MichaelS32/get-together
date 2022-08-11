857e34a21997c4e964718830e7c0367750f5c686
mschneider <michaelschneider3232@gmail.com>
2022-08-11 12:24:56 -0500

    fixing errors

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment, Vote, Event } = require('../models');
+const { User, Comment, Event } = require('../models');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
--- server.js
+++ server.js
@@ -27,6 +27,7 @@ const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
