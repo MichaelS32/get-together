cbc894d5a90e29b99bdb568079a402e8aa1d7de8
mschneider <michaelschneider3232@gmail.com>
2022-08-11 17:28:21 -0500

    still nothing

--- server.js
+++ server.js
@@ -36,5 +36,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(process.env.PORT, () => console.log(`Now listening on ${PORT}`));
+    app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
